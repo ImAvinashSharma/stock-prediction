e4caba08136563a0d8e68861bdeebe2f4d5532f8
avi9360 <savinash406@gmail.com>
2022-06-25 22:16:34 +0530

    Stock Prediction App using streamlit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # stock-prediction
+Rum Cmd - stock prediction
+
+```sh
+pip install -r requirements.txt
+streamlit run main.py
+```
---
+++ main.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import yfinance as yf
+#fast loading of th data
+yf.pdr_override()
+import pandas as pd
+import cufflinks as cf
+import datetime
+from plotly import graph_objs as go
+from datetime import date
+# Ta
+import numpy as np
+import matplotlib.pyplot as plt
+from ta.volatility import BollingerBands
+from ta.trend import MACD
+from ta.momentum import RSIIndicator
+
+#cache the data set
+import requests_cache
+session = requests_cache.CachedSession('yfinance.cache')
+session.headers['User-agent'] = 'my-program/1.0'
+
+
+st.set_page_config(
+  page_title="Poly-Invest",
+  layout="centered",
+  page_icon="❤️",
+  initial_sidebar_state="expanded",
+)
+st.header("Stock & Crypto Prediction App")
+
+
+# Sidebar
+st.sidebar.subheader('Query parameters')
+start_date = st.sidebar.date_input("Start date", datetime.date(2021, 1, 1))
+end_date = st.sidebar.date_input("End date", datetime.date.today())
+
+#valid date checker
+if start_date < end_date:
+  st.sidebar.success('Start date: `%s`\n\nEnd date: `%s`' % (start_date, end_date))
+else:
+  st.sidebar.error('Error: End date must fall after start date.')
+
+# Retrieving tickers data
+ticker_list = pd.read_csv('data-stock.txt')
+tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list) # Select ticker symbol
+tickerDuration = st.sidebar.selectbox('Time', ('1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'))
+tickerData = yf.Ticker(tickerSymbol,session=session) # Get ticker data
+tickerDf = tickerData.history(period=tickerDuration,interval="1d", start=start_date, end=end_date)
+df = yf.download(tickerSymbol,start=start_date, end=end_date)
+#get the historical prices for this ticker
+
+st.header("**About Company**")
+
+# Ticker information
+string_logo = '<img src=%s>' % tickerData.info['logo_url']
+st.markdown(string_logo, unsafe_allow_html=True)
+
+cmp_name = tickerData.info['longName']
+st.header('**%s**' % cmp_name)
+
+string_summary = tickerData.info['longBusinessSummary']
+st.info(string_summary)
+
+# Bollinger bands
+st.header('**Bollinger Bands**')
+qf=cf.QuantFig(df,title='First Quant Figure',legend='top',name='GS')
+qf.add_bollinger_bands()
+fig = qf.iplot(asFigure=True)
+st.plotly_chart(fig)
+
+# Moving Average Convergence Divergence
+macd = MACD(df['Close']).macd()
+
+# Resistence Strength Indicator
+rsi = RSIIndicator(df['Close']).rsi()
+
+
+# Plot MACD
+st.write('Stock Moving Average Convergence Divergence (MACD)')
+st.area_chart(macd)
+
+# Plot RSI
+st.write('Stock RSI ')
+st.line_chart(rsi,use_container_width=True)
+
+
+
+st.write(df)
+# News
+st.header("**News About: " + cmp_name +"**")
+for info in tickerData.news:
+  st.write("<a target='_blank' href='"+info['link']+"'>"+info["title"]+"</a>",unsafe_allow_html=True)
+
+
+# Api
+st.write('---')
+st.write(tickerData.info)
+
+
+#TODO remover unwanted line
